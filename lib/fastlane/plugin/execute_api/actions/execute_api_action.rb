d277f323217233142153b825de7922e0b2253150
Pasan Eramusugoda <pasan.eramusugoda@axienta.lk>
2022-01-20 15:51:26 +0530

    fixes

--- lib/fastlane/plugin/execute_api/actions/execute_api_action.rb
+++ lib/fastlane/plugin/execute_api/actions/execute_api_action.rb
@@ -68,7 +68,7 @@ module Fastlane
         response = request.execute
         UI.message(response)
         if response.code == 200 || response.code == 201
-          if params[:uploadArtifacts]?
+          if params[:uploadArtifacts]
             UI.success("Successfully finished uploading the fille")
           else
             UI.success("Successfully finished executing the request")
